0cfa8c08098ffb6064f19006f50f16a6053178bc
ntnrajput <ntn.rajput89@gmail.com>
2023-09-24 09:59:29 -0700

    all

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 const app = express();
 const port = process.env.PORT || 3000;
